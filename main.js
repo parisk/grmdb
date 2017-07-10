897ef3449b897a88ad6d2bce503d8982c35bf3f4
Paris Kasidiaris <paris@sourcelair.com>
2017-07-10 21:21:58 +0300

    Add descriptive comments and LICENSE

--- main.js
+++ main.js
@@ -1,56 +1,117 @@
+// We run all of our code in a `$()`, to make it run, after the DOM has loaded.
+// `$()` is a shorthand for `$.ready()`: https://api.jquery.com/ready/
+
 $(function () {
+
+  // When a user clicks on the DOM element with id="search-button", then
+  // run the code included in the provided anonymous function.
   $('#search-button').on('click', function () {
+
+    // Let the user know that they have to wait a little bit :).
+    $('#movie-list').text('Πλιζ γουέιτ...');
+
+    // Request the `/api/movies.json` file from the server
     $.get('/api/movies.json')
+
+       // If the request succeeds, then run the code included in the provided
+       // anonymous function. The value of `data` will be a JavaScript array
+       // that will contain all entries of `movies.json`.
       .done(function (data) {
-      $('#movie-list').html('');
-      for (let i=0; i<data.length; i++) {
-        let movie = data[i];
 
-        let startsWith = $('#movie-search').val();
-        let titleStartsWith = movie.title.substring(
-          0, startsWith.length
-        );
+        // Empty the contents of the DOM element with id="movie-list"
+        $('#movie-list').html('');
 
-        if (startsWith != titleStartsWith) {
-          continue;
-        }
+        // Run a for loop with {number of movies} number of steps
+        for (let i=0; i<data.length; i++) {
 
+          // Store the current movie in a local variable, as a shorthand
+          let movie = data[i];
 
+          // Store the current search term in a local variable
+          let startsWith = $('#movie-search').val();
 
+          // Store the first {search term length} number of characters of
+          // the current movie into a local variable
+          let titleStartsWith = movie.title.substring(
+            0, startsWith.length
+          );
 
+          // If `startsWith` == `titleStartsWith`, which in simple words means:
+          //
+          // If the current movie's title starts **exactly** with the current
+          // search term, run the code inside the `if` clause.
+          if (startsWith == titleStartsWith) {
 
+            // Start creating the HTML code needed for a `list-group-item` to
+            // display the current movie, in the movie list/
+            //
+            // http://getbootstrap.com/components/#list-group
+            let movieHtml = '<li class="list-group-item">';
+            movieHtml += movie.title;
 
-        let movieHtml = '<li class="list-group-item">';
-        movieHtml += movie.title;
-        movieHtml += '<button data-movie-id="';
-        movieHtml += movie.id;
-        movieHtml += '" data-toggle="modal" data-target="#myModal">Επισκόπηση</button>';
-        movieHtml += '</li>';
+            // Store the current movie's id in a data attribute in each
+            // list item's "inspect" button"
+            // https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
+            movieHtml += '<button data-movie-id="';
+            movieHtml += movie.id;
+            movieHtml += '" data-toggle="modal" data-target="#film-details">Επισκόπηση</button>';
+            movieHtml += '</li>';
 
-        $('#movie-list').append(movieHtml);
-      };
+            // Add the HTML element for the current film in the movie list
+            // http://getbootstrap.com/javascript/#live-demo
+            $('#movie-list').append(movieHtml);
+          }
+        };
     });
   });
 });
 
 
-$('#myModal').on('show.bs.modal', function (event) {
-  let button = $(event.relatedTarget); // Button that triggered the modal
+// When the movie details modal window opens run the code included in the
+// provided anonymous function.
+// http://getbootstrap.com/javascript/#modals-events
+$('#film-details').on('show.bs.modal', function (event) {
+
+  // Store the button that triggered the modal into a local variable
+  let button = $(event.relatedTarget);
+
+  // Store the movie id data attribute of the above button
+  // into a local variable
   let movieId = parseInt($(button).attr('data-movie-id'));
 
-  console.log(
-    'Modal opened from button of movie ' + $(button).attr('data-movie-id')
-  );
-
+  // Request the `/api/movies.json` file from the server
   $.get('/api/movies.json')
+
+    // If the request succeeds, then run the code included in the provided
+    // anonymous function. The value of `data` will be a JavaScript array
+    // that will contain all entries of `movies.json`.
     .done(function (data) {
+
+      // Run a for loop with {number of movies} number of steps
       for (let i=0; i<data.length; i++) {
+
+        // Store the current movie in a local variable, as a shorthand
         let movie = data[i];
 
+        // If the current movie's id equals to the id of the button that
+        // triggered the modal, run the code included in the following
+        // `if` clause.
         if (movie.id == movieId) {
+
+          // Set the `src` attribute of the movie's image element,
+          // to the `image_url` attribute of the current movie object.
           $('#movie-img').attr('src', movie.image_url);
+
+          // Set the content of the movie's title element
+          // to the `title` attribute of the current movie object.
           $('#movie-title').text(movie.title);
+
+          // Set the content of the movie's rating element
+          // to the `rating` attribute of the current movie object.
           $('#movie-rating').text(movie.rating);
+
+          // Set the content of the movie's description element
+          // to the `description` attribute of the current movie object.
           $('#movie-description').text(movie.description);
         }
       }
